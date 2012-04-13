3e988d15d5c741bfb143c14bafa98126a502dbe7
Martin Landalv <martin@landalv.se>
2012-04-13 19:30:11 +0200

    Renamed rules to methods. Added date, dateISO, email and url methods.

---
+++ src/methods.js
@@ -0,0 +1,60 @@
+(function (ko) {
+    "use strict";
+
+    var utils = ko.validator.utils;
+
+    ko.validator.addMethod("required",
+        function (value) {
+            return value !== undefined && value !== null && value !== "";
+        }, "This field is required.");
+
+    ko.validator.addMethod("number",
+        function (value, target) {
+            return this.optional(target) || !isNaN(+(value));
+        }, "Please enter a valid number.");
+
+    ko.validator.addMethod("min",
+        function (value, target, param) {
+            return this.optional(target) || value >= param;
+        }, "Please enter a value greater than or equal to {0}.");
+
+    ko.validator.addMethod("max",
+        function (value, target, param) {
+            return this.optional(target) || value <= param;
+        }, "Please enter a value less than or equal to {0}.");
+
+    ko.validator.addMethod("digits",
+        function (value, target) {
+            return this.optional(target) || /^\d+$/.test(value);
+        }, "Please enter only digits.");
+
+    ko.validator.addMethod("range",
+        function (value, target, param) {
+            return this.optional(target) || (value >= param.min && value <= param.max);
+        }, function (param) {
+            return utils.format("Please enter a value between {0} and {1}.", param.min, param.max);
+        });
+
+    ko.validator.addMethod("date",
+        function (value, target) {
+            // From jQuery Validation
+            return this.optional(target) || !/Invalid|NaN/.test(new Date(value));
+        }, "Please enter a valid date.");
+
+    ko.validator.addMethod("dateISO",
+        function (value, target) {
+            return this.optional(target) || /^\d{4}[\/\-]\d{2}[\/\-]\d{2}$/.test(value);
+        }, "Please enter a valid date (ISO).");
+
+    ko.validator.addMethod("url",
+        function (value, target) {
+            // From jQuery Validation
+            return this.optional(target) || /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(value);
+        }, "Please enter a valid URL.");
+
+    ko.validator.addMethod("email",
+        function (value, target) {
+            // From jQuery Validation
+            return this.optional(target) || /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(value);
+        }, "Please enter a valid email address.");
+}(ko));
---
+++ test/methods.js
@@ -0,0 +1,271 @@
+/// <reference path="../libs/qunit-git.js" />
+/// <reference path="../libs/knockout-2.0.0.js" />
+/// <reference path="../build/output/knockout-validate-debug.js" />
+
+module("Methods");
+
+test("required", function () {
+    var target = ko.observable(),
+        method = ko.validator.methods.required;
+
+    equal(ko.validator.messages.required, "This field is required.", "Verify default message");
+
+    // undefined
+    equal(method(target(), target), false, "Expects false when value is undefined");
+
+    // null
+    target(null);
+    equal(method(target(), target), false, "Expects false when value is null");
+
+    // Empty string
+    target("");
+    equal(method(target(), target), false, "Expects false when value is empty string");
+
+    // Zero is falsy, but a valid value
+    target(0);
+    equal(method(target(), target), true, "Expects true if value is 0");
+
+    // False
+    target(false);
+    equal(method(target(), target), true, "Expects true when value is false");
+
+    // Non-zero length string
+    target("a");
+    equal(method(target(), target), true, "Expects true when string value is set");
+});
+
+test("number", function () {
+    var target = ko.observable(),
+        method = ko.validator.methods.number;
+
+    equal(ko.validator.messages.number, "Please enter a valid number.", "Verify default message");
+    equal(method(target(), target), true, "True if value is not set (optional)");
+
+    // Zero
+    target(0);
+    equal(method(target(), target), true, "True is returned if value is 0");
+
+    // String
+    target("foobar");
+    equal(method(target(), target), false, "False if value not convertable to number");
+
+    // String decimals
+    target("0.1");
+    equal(method(target(), target), true, "True if decimal value")
+    target("0,1");
+    equal(method(target(), target), false, "False if invalid decimal format");
+});
+
+test("min", function () {
+    var target = ko.observable(),
+        method = ko.validator.methods.min;
+
+    equal(ko.validator.messages.min, "Please enter a value greater than or equal to {0}.", "Verify default message");
+    equal(method(target(), target), true, "True if value is not set (optional)");
+
+    // String
+    target("foobar");
+    equal(method(target(), target, 1), false, "False if value not convertable to number");
+    target("10");
+    equal(method(target(), target, 5), true, "True if value convertable to number and greater than min");
+
+    target(0);
+    equal(method(target(), target, 1), false, "False if value less than min");
+    equal(method(target(), target, 0), true, "True if value equal to min");
+    equal(method(target(), target, -1), true, "True if value greater than min");
+});
+
+test("max", function () {
+    var target = ko.observable(),
+        method = ko.validator.methods.max;
+
+    equal(ko.validator.messages.max, "Please enter a value less than or equal to {0}.", "Verify default message");
+    equal(method(target(), target), true, "True if value is not set (optional)");
+
+    // String
+    target("foobar");
+    equal(method(target(), target, 1), false, "False if value not convertable to number");
+    target("5");
+    equal(method(target(), target, 10), true, "True if value convertable to number and less than max");
+
+    target(0);
+    equal(method(target(), target, 1), true, "True if value less than max");
+    equal(method(target(), target, 0), true, "True if value equal to max");
+    equal(method(target(), target, -1), false, "False if value greater than max");
+});
+
+test("digits", function () {
+    var target = ko.observable(),
+        method = ko.validator.methods.digits;
+
+    equal(ko.validator.messages.digits, "Please enter only digits.", "Verify default message");
+    equal(method(target(), target), true, "True if value is not set (optional)");
+
+    // Zero
+    target(0);
+    equal(method(target(), target), true, "True if integer");
+
+    // String
+    target("foobar");
+    equal(method(target(), target), false, "False if value not convertable to number");
+
+    target("0.1");
+    equal(method(target(), target), false, "False if decimal value")
+    target(0.1);
+    equal(method(target(), target), false, "False if decimal value")
+    target("0,1");
+    equal(method(target(), target), false, "False if invalid decimal separator (parseInt would parse this to a valid digit number)");
+});
+
+test("range", function () {
+    var target = ko.observable(),
+        method = ko.validator.methods.range;
+
+    // Insert param values to test default values since function is used
+    equal(ko.validator.messages.range({ min: 1, max: 2 }), "Please enter a value between 1 and 2.", "Verify default message");
+    equal(method(target(), target), true, "True if value is not set (optional)");
+
+    target(1);
+    equal(method(target(), target, { min: 1, max: 2 }), true, "True if value equal to min");
+    target(2);
+    equal(method(target(), target, { min: 1, max: 2 }), true, "True if value equal to max");
+    target(2);
+    equal(method(target(), target, { min: 2, max: 2 }), true, "True if value equal to min and max");
+
+    target(0);
+    equal(method(target(), target, { min: 1, max: 2 }), false, "False if value less than min");
+    target(3);
+    equal(method(target(), target, { min: 1, max: 2 }), false, "False if value greater than max");
+
+    target("foobar");
+    equal(method(target(), target, { min: 1, max: 2 }), false, "False if value not number");
+});
+
+test("date", function () {
+    var target = ko.observable(),
+        method = ko.validator.methods.date;
+
+    equal(ko.validator.messages.date, "Please enter a valid date.", "Verify default message");
+    equal(method(target(), target), true, "True if value is not set (optional)");
+
+    target(new Date());
+    equal(method(target(), target), true, "True if date object");
+
+    target(0);
+    equal(method(target(), target), true, "True if 0");
+
+    target("");
+    equal(method(target(), target), true, "True if empty string (optional)");
+
+    target("2011-01-01");
+    equal(method(target(), target), true, "True if ISO format");
+
+    target("July 27, 2005 12:22:00");
+    equal(method(target(), target), true, "True if correct string format for Date constructor");
+
+    target("foobar");
+    equal(method(target(), target), false, "False if non date string");
+});
+
+test("dateISO", function () {
+    var target = ko.observable(),
+        method = ko.validator.methods.dateISO;
+
+    equal(ko.validator.messages.dateISO, "Please enter a valid date (ISO).", "Verify default message");
+    equal(method(target(), target), true, "True if value is not set (optional)");
+
+    target("2000-01-01");
+    equal(method(target(), target), true, "True if yyyy-MM-dd");
+
+    target("0000-01-01");
+    equal(method(target(), target), true, "True if year 0");
+
+    equal(method(target(), target), true, "True if year -1");
+
+    target("20000101");
+    equal(method(target(), target), false, "False if dashes omitted");
+
+    target("90-01-01");
+    equal(method(target(), target), false, "False if year only is 2 characters");
+
+    target("2000-1-01");
+    equal(method(target(), target), false, "False if no leading 0 in month");
+
+    target("2000-01-1");
+    equal(method(target(), target), false, "False if no leading 0 in day");
+});
+
+test("url", function () {
+    var target = ko.observable(),
+        method = ko.validator.methods.url;
+
+    equal(ko.validator.messages.url, "Please enter a valid URL.", "Verify default message");
+    equal(method(target(), target), true, "True if value is not set (optional)");
+
+    target("foobar");
+    equal(method(target(), target), false, "False if foobar");
+
+    target(true);
+    equal(method(target(), target), false, "False if boolean");
+    target(false);
+    equal(method(target(), target), false, "False if boolean");
+
+    target(0);
+    equal(method(target(), target), false, "False if number");
+    target(1.1);
+    equal(method(target(), target), false, "False if number");
+
+    target("http");
+    equal(method(target(), target), false, "False if only protocol");
+
+    target("http://");
+    equal(method(target(), target), false, "False if only protocol");
+
+    target("http://google");
+    equal(method(target(), target), false, "False if TLD missing");
+
+    target("http://google.com");
+    equal(method(target(), target), true, "True if no trailing slash");
+
+    target("http://google.com/");
+    equal(method(target(), target), true, "True if trailing slash");
+
+    target("https://google.com/");
+    equal(method(target(), target), true, "True if https protocl");
+
+    target("ftp://google.com/");
+    equal(method(target(), target), true, "True if ftp protocol");
+});
+
+test("email", function () {
+    var target = ko.observable(),
+        method = ko.validator.methods.email;
+
+    equal(ko.validator.messages.email, "Please enter a valid email address.", "Verify default message");
+    equal(method(target(), target), true, "True if value is not set (optional)");
+
+    target("foobar");
+    equal(method(target(), target), false, "False if foobar");
+
+    target(true);
+    equal(method(target(), target), false, "False if boolean");
+    target(false);
+    equal(method(target(), target), false, "False if boolean");
+
+    target(0);
+    equal(method(target(), target), false, "False if number");
+    target(1.1);
+    equal(method(target(), target), false, "False if number");
+
+    target("user@");
+    equal(method(target(), target), false, "False if domain is missing");
+
+    target("user@domain");
+    equal(method(target(), target), false, "False if TLD is missing");
+
+    target("user@domain.com");
+    equal(method(target(), target), true, "True if simple email");
+
+    target("u-s.er@domain.com");
+    equal(method(target(), target), true, "True if address contains hyphen and dot");
+});
